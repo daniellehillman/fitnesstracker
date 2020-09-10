532b20293340535c81f387fb263ea591f5bde1bb
Danielle Hillman <hillm108@mail.chapman.edu>
2020-09-10 16:55:38 -0700

    git ignore

---
+++ models/Workout.js
@@ -0,0 +1,38 @@
+const { model, Schema } = require('mongoose')
+const Workout = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [{
+    name: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    weight: {
+      type: Number,
+      required: false
+    },
+    sets: {
+      type: Number,
+      required: false
+    },
+    reps: {
+      type: Number,
+      required: false
+    },
+    duration: {
+      type: Number,
+      required: true
+    },
+    distance: {
+      type: Number,
+      required: false
+    }
+  }]
+}, { timestamps: true })
+module.exports = model('Workout', Workout)
